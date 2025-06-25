410a1a23c13eb4e39e16f7cc1fe1b0d471afc397
iskur <dirk.schlabing@posteo.de>
2025-06-25 13:29:52 +0200

    docs: Revert to alabaster theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -145,8 +145,8 @@ pygments_style = "sphinx"
 # html_theme = 'default'
 # html_theme = "classic"
 # html_theme = "sphinxdoc"
-# html_theme = "alabaster"
-html_theme = 'sphinx_rtd_theme'
+html_theme = "alabaster"
+# html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
